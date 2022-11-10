15b23fa7b5cd96d6d197ab12f77a4f6f7c6cb495
jmorrissey23 <92754920+jmorrissey23@users.noreply.github.com>
2022-11-09 23:19:27 -0600

    Updated graph.h

--- src/graph.h
+++ src/graph.h
@@ -1,6 +1,25 @@
 #pragma once
+#include <vector> 
+#include <string> 
+
+using namespace std; 
 
 class Graph {
+    public:
+        Graph(string articles, string links); 
+        void removeVertex(Node v);
+        vector<pair<int, Node>> incidentEdges(Node v); //returns the list of edges adjacent to node v
+        bool areAdjacent(Node v1, Node v2); 
+        void insertEdge(Node v1, Node v2, int weight); // Weight = v1.degree + v2.degree
+        
     private:
-    void readFromFile();
+        struct Node {
+            Node(string article) : data(article); // Node constructor
+            vector<pair<int, Node> adjList;  // list of adjacent nodes and their edge weights
+            pair<int, Node> connection;  // int: the weight of the edge, Node: the adjacent node connected by the edge
+            string data; // Article name
+            int degree; // Number of total edges connected to node (adjList.size())
+        };
+        void readFromFile();
+        vector<Node> nodeList_;    
 };
